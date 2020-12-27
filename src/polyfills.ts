224d042e7ba5a63e6e03dc7c0ada3ac21214480a
Jeremy May <may.jeremy.r@gmail.com>
2020-12-27 12:31:15 -0800

    workaround for compatibility issue

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+global.Buffer = global.Buffer || require('buffer').Buffer;
+(window as any).process = {
+  version: ''
+};
